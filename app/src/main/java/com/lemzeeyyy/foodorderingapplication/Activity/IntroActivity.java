66ecbd1e3040e24726bd3486e2f4cc473ee8805e
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-06-26 17:45:09 +0100

    intro activity ui

---
+++ app/src/main/java/com/lemzeeyyy/foodorderingapplication/Activity/IntroActivity.java
@@ -0,0 +1,28 @@
+package com.lemzeeyyy.foodorderingapplication.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.lemzeeyyy.foodorderingapplication.R;
+
+public class IntroActivity extends AppCompatActivity {
+    private ConstraintLayout startBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        startBtn = findViewById(R.id.get_started_btn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IntroActivity.this, MainActivity.class));
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/lemzeeyyy/foodorderingapplication/Adapter/CategoryAdapter.java
@@ -0,0 +1,4 @@
+package com.lemzeeyyy.foodorderingapplication.Adapter;
+
+public class CategoryAdapter {
+}
