766d1a7149d8b0867137b9a6e855cd05155dfc70
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-06-26 17:30:27 +0100

    main activity ui

--- app/src/main/java/com/lemzeeyyy/foodorderingapplication/Activity/MainActivity.java
+++ app/src/main/java/com/lemzeeyyy/foodorderingapplication/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.lemzeeyyy.foodorderingapplication;
+package com.lemzeeyyy.foodorderingapplication.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.lemzeeyyy.foodorderingapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
